69ffda9d584c67c7d3d2d59dd259a115ae76ffc2
Jasmine Chen <j.lin013@gmail.com>
2016-03-07 01:19:39 +0800

    Completed parsing

--- chdict.py
+++ chdict.py
@@ -14,11 +14,20 @@ def renew_session():
 	a = soup.find('a')['href']
 	ccd = a[a.find('ccd')+4:].split('&')[0]
 
+def parse_soup(soup):
+	fields = soup.find_all('th', class_='std1')
+	# print(len(fields))
+	for field in fields:
+		if field.b.string.find('釋義') != -1:
+			return field.next_sibling.get_text()
+
 def parse_page(url):
 	global ccd, ck
-	print(url)
+	# print(url)
 	res = requests.get(url, cookies={'new_www_edu_Tw': ck})
 	# print(res.text)
+	soup = BeautifulSoup(res.text, 'html.parser')
+	return parse_soup(soup)
 
 def lookup(word):
 	payload = {'o': 'e0', 'ccd': ccd, 'sec': 'sec1', 'qs0': word, 'clscan': '', 'selectmode': 'mode1', 'button.x': '0', 'button.y': '0', 'button': '提交'}
@@ -33,7 +42,8 @@ def lookup(word):
 	if (num == 0):
 		ret = ''
 	elif (num == 1):
-		ret = 'Single result'
+		data = parse_soup(soup)
+		print(data)
 	else:
 		''' find links the proper way - probably better if new modes are added
 		maintds = soup.find_all('td', class_=re.compile('maintd'))
@@ -41,9 +51,10 @@ def lookup(word):
 			if td.a != None:
 				print(td.a['href'])
 		'''
-		parse_page('http://dict.revised.moe.edu.tw/cgi-bin/cbdic/gsweb.cgi?ccd=%s&o=e0&sec=sec1&op=v&view=0-1' %(ccd))
+		data = parse_page('http://dict.revised.moe.edu.tw/cgi-bin/cbdic/gsweb.cgi?ccd=%s&o=e0&sec=sec1&op=v&view=0-1' %(ccd))
+		print(data)
 
 renew_session()
 # print(ccd)
-lookup('丁')
+lookup('哈哈')
 

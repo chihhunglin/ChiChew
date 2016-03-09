3ff0fdd98e9d53c162228bd2665c92d846c6409a
Jasmine Chen <j.lin013@gmail.com>
2016-03-10 05:29:54 +0800

    Added option to customize output format (and information)

--- chdict.py
+++ chdict.py
@@ -9,12 +9,16 @@ output_type = 0
 ccd = None
 ck = None
 results = []
+fmt_str = '%w：%m'
 
 parser = argparse.ArgumentParser(description='Chinese-Chinese Dictionary Crawler')
 parser.add_argument('keywords', nargs=argparse.REMAINDER)
+parser.add_argument('-s', '--strformat', type=str)
 parser.add_argument('-f', '--file', type=str)
 parser.add_argument('-o', '--output', type=str)
 args = vars(parser.parse_args())
+if args['strformat'] != None:
+	fmt_str = args['strformat']
 if args['file'] != None:
 	input_type = 1
 if args['output'] != None:
@@ -30,9 +34,17 @@ def renew_session():
 
 def parse_soup(soup):
 	fields = soup.find_all('th', class_='std1')
+	ret = dict()
 	for field in fields:
-		if field.b.string.find('釋義') != -1:
-			return field.next_sibling.get_text()
+		if field.b.string.find('字詞') != -1:
+			ret['word'] = field.next_sibling.get_text().strip('【】')
+		elif field.b.string.find('注音') != -1:
+			ret['bopomofo'] = field.next_sibling.get_text()
+		elif field.b.string.find('拼音') != -1:
+			ret['pinyin'] = field.next_sibling.get_text()
+		elif field.b.string.find('釋義') != -1:
+			ret['meaning'] = field.next_sibling.get_text()
+	return ret
 
 def parse_page(url):
 	global ccd, ck
@@ -40,14 +52,21 @@ def parse_page(url):
 	soup = BeautifulSoup(res.text, 'html.parser')
 	return parse_soup(soup)
 
+def get_resultstr(result):
+	tmp = fmt_str;
+	return tmp.replace('%w', result['word']) \
+		.replace('%b', result['bopomofo']) \
+		.replace('%p', result['pinyin']) \
+		.replace('%m', result['meaning'])
+
 def print_results():
 	if output_type == 0:
-		for pair in results:
-			print('%s：%s' %(pair['word'], pair['data']))
+		for result in results:
+			print(get_resultstr(result))
 	elif output_type == 1:
 		fout = open(args['output'], 'w')
-		for pair in results:
-			fout.write('%s：%s\n' %(pair['word'], pair['data']))
+		for result in results:
+			fout.write(get_resultstr(result) + '\n')
 		fout.close()
 
 def lookup(word):
@@ -59,10 +78,12 @@ def lookup(word):
 		num = 1
 	else:
 		num = int(tmp.find('font', class_='numfont').string)
+	
+	res = dict()
 	if (num == 0):
-		data = '沒有資料'
+		res = {'word': word, 'bopomofo': '', 'pinyin': '', 'meaning': ''}
 	elif (num == 1):
-		data = parse_soup(soup)
+		res = parse_soup(soup)
 	else:
 		''' find links the proper way - probably better if new modes are added
 		maintds = soup.find_all('td', class_=re.compile('maintd'))
@@ -70,9 +91,8 @@ def lookup(word):
 			if td.a != None:
 				print(td.a['href'])
 		'''
-		data = parse_page('http://dict.revised.moe.edu.tw/cgi-bin/cbdic/gsweb.cgi?ccd=%s&o=e0&sec=sec1&op=v&view=0-1' %(ccd))
-	# print('%s：%s' %(word, data))
-	results.append({'word': word, 'data': data})
+		res = parse_page('http://dict.revised.moe.edu.tw/cgi-bin/cbdic/gsweb.cgi?ccd=%s&o=e0&sec=sec1&op=v&view=0-1' %(ccd))
+	results.append(res)
 
 renew_session()
 
